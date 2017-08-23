d16169146f991de3e12be48e147b364beff655fb
Moris Mulyashov <mulyashov.moris@gmail.com>
2017-08-23 10:45:13 +0300

    remove cross-env

--- server/config/webpackConfig.js
+++ server/config/webpackConfig.js
@@ -4,13 +4,11 @@ import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../../webpack.config';
 
 export default (app) => {
-	if (process.env.NODE_ENV === 'development'){
-		const compiler = webpack(webpackConfig);
-		app.use(webpackMiddleware(compiler, {
-			hot: true,
-			publicPath: webpackConfig.output.publicPath,
-			noInfo: true
-		}));
-		app.use(webpackHotMiddleware(compiler));
-	}
+	const compiler = webpack(webpackConfig);
+	app.use(webpackMiddleware(compiler, {
+		hot: true,
+		publicPath: webpackConfig.output.publicPath,
+		noInfo: true
+	}));
+	app.use(webpackHotMiddleware(compiler));
 };
